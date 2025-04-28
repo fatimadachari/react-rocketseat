8eed11b25edf3410d6fefbf173cc6b5809006c8d
fatimadachari <fatima.s.dachari@gmail.com>
2025-04-28 16:50:27 -0300

    criando a sidebar

--- ignite-feed/src/App.jsx
+++ ignite-feed/src/App.jsx
@@ -1,9 +1,18 @@
 import { Header } from "./components/Header";
 import './global.css';
-
+import styles from './App.module.css'
+import { Sidebar } from "./components/Sidebar";
 export function App() {
 
   return (
-    <Header />
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <aside>
+          <Sidebar/>
+        </aside>
+        <main>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus magni tempore ex asperiores! Error id corrupti similique rem impedit sint pariatur architecto quo eos, distinctio eum fugit doloribus cumque dolor.</main>
+      </div>
+    </div>
   )
 }
